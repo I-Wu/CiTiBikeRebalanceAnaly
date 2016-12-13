596f4780c64bdf72337a7a453c0a6aeb92756fb8
I-Wu <eddie@172-16-197-248.DYNAPOOL.NYU.EDU>
2016-12-12 22:15:50 -0500

    Update the code
    We are good.

--- AnalyScripts2/Weather_station.sql
+++ AnalyScripts2/Weather_station.sql
@@ -1,6 +1,30 @@
-CREATE TABLE tripwea10_v1 AS SELECT a.row_num, a.tripd, a.startt, a.stopt,
+CREATE TABLE tripwea02_v10 AS SELECT a.row_num, a.tripd, a.startt, a.stopt, a. startstationid AS sid, a.endstationid AS eid,
          b.newtime, b.wtype, b.temp
-            FROM  triptable10_v2 a
-            JOIN 201510_wea_v4 b
+            FROM  triptable02_v2 a
+            JOIN  201602_wea_v4 b
             WHERE b.newtime  >= a.startt
-             AND  b.newtime  <= a.stopt;
+            AND  b.newtime  <= a.stopt;
+
+CREATE TABLE tripwea02_v11 AS SELECT * FROM tripwea02_v10 where array_contains(split(wtype,' '),'-RA');
+
+CREATE TABLE tripwea02_v12 AS SELECT row_num, tripd, sid, eid, COUNT(distinct newtime) AS counter
+FROM tripwea02_v11
+GROUP BY row_num,tripd, sid, eid;
+
+CREATE TABLE tripweaRank02_v1 AS SELECT
+sid, count(*) as outflow FROM tripwea02_v12 GROUP BY sid ORDER by outflow DESC;
+
+CREATE TABLE tripweaRank02_v2 AS SELECT
+eid, count(*) as inflow FROM tripwea02_v12 GROUP BY eid ORDER by inflow DESC;
+
+CREATE TABLE tripwea02_v13 AS SELECT * FROM tripwea02_v10 where array_contains(split(wtype,' '),'+RA');
+
+CREATE TABLE tripwea02_v14 AS SELECT row_num, tripd, sid, eid, COUNT(distinct newtime) AS counter
+FROM tripwea02_v13
+GROUP BY row_num,tripd, sid, eid;
+
+CREATE TABLE tripweaRank02_v3 AS SELECT
+sid, count(*) as outflow FROM tripwea02_v14 GROUP BY sid ORDER by outflow DESC;
+
+CREATE TABLE tripweaRank02_v4 AS SELECT
+eid, count(*) as inflow FROM tripwea02_v14 GROUP BY eid ORDER by inflow DESC;
